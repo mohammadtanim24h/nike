15233adec42b5454525cf960129d7170ff790af2
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2024-12-22 00:04:29 +0600

    updated the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { headerLogo } from "../assets/images";
-import { hamburger } from "../assets/icons";
+import { hamburger, close } from "../assets/icons";
 import { navLinks } from "../constants";
 
 const Nav = () => {
@@ -16,11 +16,14 @@ const Nav = () => {
                 <a href="/">
                     <img src={headerLogo} alt="Logo" width={130} height={29} />
                 </a>
-                <ul className="flex justify-center items-center gap-16 max-lg:hidden">
+                <ul className="flex justify-center items-center max-lg:hidden">
                     {navLinks.map((item) => (
-                        <li key={item.label}>
+                        <li
+                            key={item.label}
+                            className="nav-link"
+                        >
                             <a
-                                className="font-montserrat leading-normal text-lg text-slate-gray"
+                                className="font-montserrat leading-normal text-lg font-medium"
                                 href={item.href}
                             >
                                 {item.label}
@@ -29,13 +32,38 @@ const Nav = () => {
                     ))}
                 </ul>
                 <div className="lg:hidden">
-                    <img
-                        src={hamburger}
-                        alt="hamburger"
-                        width={25}
-                        height={25}
-                        onClick={toggleHamburgerMenu}
-                    />
+                    {!displayMobileMenu && (
+                        <img
+                            src={hamburger}
+                            alt="hamburger"
+                            width={25}
+                            height={25}
+                            onClick={toggleHamburgerMenu}
+                        />
+                    )}
+                    {displayMobileMenu && (
+                        <div className="fixed bg-white top-0 right-0 w-9/12 h-screen z-11 shadow-xl padding-x py-8 transition-all ease-in-out duration-500">
+                            <img
+                                src={close}
+                                alt="close sidebar"
+                                className="mb-4 ml-24"
+                                onClick={toggleHamburgerMenu}
+                            />
+
+                            <ul className="flex flex-col space-y-2">
+                                {navLinks.map((item) => (
+                                    <li key={item.label}>
+                                        <a
+                                            className="font-montserrat leading-normal text-lg text-slate-gray font-medium"
+                                            href={item.href}
+                                        >
+                                            {item.label}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </nav>
         </header>
