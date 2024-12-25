6586662af65a701ecd1f7fc2e5b193e1a4cf97f1
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2024-12-25 09:56:27 +0600

    updated popular product card appearance

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -2,9 +2,9 @@ import { star } from "../assets/icons";
 
 const PopularProductCard = ({ imgURL, name, price, rating }) => {
     return (
-        <div className="flex flex-1 flex-col max-sm:w-full">
-            <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
-            <div className="px-2">
+        <div className="flex flex-1 flex-col max-sm:w-full shadow-md rounded-3xl">
+            <img src={imgURL} alt={name} className="w-full object-cover" />
+            <div className="px-4 pb-4">
                 <div className="mt-8 flex justify-start gap-2.5">
                     <img src={star} alt="rating" width={24} height={24} />
                     <p className="font-montserrat text-xl leading-normal text-slate-gray">
