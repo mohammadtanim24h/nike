60d6ade6425eccf4a6229206ea5e3fa79630fd12
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2024-12-25 10:44:56 +0600

    added super quality section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,49 @@
-const SuperQuality = () => {
-  return (
-    <div>SuperQuality</div>
-  )
-}
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
 
-export default SuperQuality
+const SuperQuality = () => {
+    return (
+        <section
+            id="about-us"
+            className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+        >
+            <div className="flex flex-1 flex-col">
+                <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+                    we provide you
+                    <br />
+                    <span className="text-coral-red inline-block mt-3">
+                        super
+                    </span>{" "}
+                    <span className="text-coral-red inline-block mt-3">
+                        quality
+                    </span>{" "}
+                    shoes
+                </h2>
+                <p className="mt-4 lg:max-w-lg info-text">
+                    Ensuring premium comfort with styles, our meticulously
+                    crafted footwear is designed to elevate your experience,
+                    providing you with unmatched quality, innovation, and a
+                    touch of elegance.
+                </p>
+                <p className="mt-6 lg:max-w-lg info-text">
+                    Our dedication to detail and excellence ensures your
+                    satisfaction.
+                </p>
+                <div className="mt-8">
+                    <Button label="View details" />
+                </div>
+            </div>
+            <div className="flex-1 flex justify-center items-center">
+                <img
+                    src={shoe8}
+                    alt="shoe"
+                    width={570}
+                    height={522}
+                    className="object-cover"
+                />
+            </div>
+        </section>
+    );
+};
+
+export default SuperQuality;
