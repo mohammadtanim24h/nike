a1b03e31b9c67c669c48c0850859fbcbc843447b
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2024-12-25 13:38:15 +0600

    added services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,14 @@
-const Services = () => {
-  return (
-    <div>Services</div>
-  )
-}
+import ServiceCard from "../components/ServiceCard";
+import { services } from "../constants";
 
-export default Services
+const Services = () => {
+    return (
+        <section className="max-container flex justify-center flex-wrap gap-9">
+            {services.map((service) => (
+                <ServiceCard key={service.label} {...service} />
+            ))}
+        </section>
+    );
+};
+
+export default Services;
