acfee0f1f19dc5af1298c347c2b04819d5d7f197
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2024-12-28 12:45:13 +0600

    added customer reviews section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,24 @@
-const CustomerReviews = () => {
-  return (
-    <div>CustomerReviews</div>
-  )
-}
+import ReviewCard from "../components/ReviewCard";
+import { reviews } from "../constants";
 
-export default CustomerReviews
+const CustomerReviews = () => {
+    return (
+        <section className="max-container">
+            <h3 className="font-palanquin text-4xl text-center font-bold">
+                What Our <span className="text-coral-red">Customers</span> Say?
+            </h3>
+            <p className="mt-4 info-text m-auto lg:max-w-lg text-center">
+                Hear genuine stories from our satisfied customers about their
+                exceptional experiences with us.
+            </p>
+
+            <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+                {reviews.map((review) => (
+                    <ReviewCard key={review.customerName} {...review} />
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default CustomerReviews;
